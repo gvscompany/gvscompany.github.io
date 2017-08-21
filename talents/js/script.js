a6fea406cf0fd44eb25fc6923e33d95604801278
Vardan <gulqanyan.vardan@gmail.com>
2017-08-21 19:12:07 +0400

    instal project

---
+++ talents/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    /* Open/Close menu */
+    $(document).on('click', '.menu-btn', function () {
+        var myMenu = $('.sidebar-menu');
+        var myContent = $('.content');
+        var links = $('.sidebar-ul > li');
+
+        if (myMenu.hasClass('in')) {
+            myMenu.removeClass('in');
+            $(this).html('<span class="glyphicon glyphicon-menu-hamburger"></span>');
+        } else {
+            myMenu.addClass('in');
+            $(this).html('<span class="glyphicon glyphicon-remove"></span>');
+        }
+        if (myContent.hasClass('out')) {
+            myContent.removeClass('out');
+        } else {
+            myContent.addClass('out');
+        }
+        if (links.hasClass('open')) {
+            links.removeClass('open');
+        } else {
+            links.addClass('open');
+        }
+    });
+    /* Open/Close menu */
+
+    $('.langs').on('click', function () {
+        $('.change-lang-text').text($(this).text());
+    });
+
+    // Input education
+    $('#education').on('change', function () {
+        if ($(this).val() != 0) {
+            $('#education').css({
+                '-webkit-appearance': 'none',
+                '-moz-appearance': 'none',
+                'appearance': 'none'
+            });
+        }
+    });
+    // Input education
+
+});
