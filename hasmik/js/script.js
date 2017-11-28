5d637dcc2e60592a3359b7ea0f64ee8384a3e7f6
gvscompany <gulqanyan.vardan@gmail.com>
2017-11-28 23:06:54 +0400

    hasmik last commit

--- hasmik/js/script.js
+++ hasmik/js/script.js
@@ -1,14 +1,13 @@
 function myFunction1() {
-    var menuBar = document.getElementById("menu_bar");
-    var iconBar = document.getElementById("icon_bar");
-    iconBar.style.display = "none";
-    if(document.body.clientWidth > 560){
-        iconBar.style.display = "none";
-        menuBar.style.display = "block";
-    } else {
-        iconBar.style.display = "block";
-
-    }
+    // var menuBar = document.getElementById("menu_bar");
+    // var iconBar = document.getElementById("icon_bar");
+    // iconBar.style.display = "none";
+    // if(document.body.clientWidth > 991){
+    //     iconBar.style.display = "none";
+    //     menuBar.style.display = "block";
+    // } else {
+    //     iconBar.style.display = "block";
+    // }
 }
 
 
@@ -22,4 +21,38 @@ function myFunction() {
         x.style.display = "none";
     }
 
+}
+
+function changeContent(n){
+    switch (n) {
+        case 1:
+            h3_content1.style.display = "block";
+            h3_content2.style.display = "none";
+            h3_content3.style.display = "none";
+            h3_content4.style.display = "none";
+            break;
+        case 2:
+            h3_content1.style.display = "none";
+            h3_content2.style.display = "block";
+            h3_content3.style.display = "none";
+            h3_content4.style.display = "none";
+            break;
+        case 3:
+            h3_content1.style.display = "none";
+            h3_content2.style.display = "none";
+            h3_content3.style.display = "block";
+            h3_content4.style.display = "none";
+            break;
+        case 4:
+            h3_content1.style.display = "none";
+            h3_content2.style.display = "none";
+            h3_content3.style.display = "none";
+            h3_content4.style.display = "block";
+            break;
+    }
+}
+
+function fixColor(){
+    this.style.background='#fff';
+    // alert("sssssssssss");
 }
