b7c992053dca8c024333ec7108b465d7fcd2963d
gulqanyan.varda <gulqanyan.varda@gmail.com>
2018-09-03 13:34:55 +0400

    commit-3

--- net/js/script.js
+++ net/js/script.js
@@ -1,6 +1,7 @@
 // PAGE SCROLL ========================
 $('a[href^="#"]').on('click', function (e) {
     e.preventDefault();
+    $('.navbar-collapse.collapse').removeClass('show');
     var currentSectionId = $(this).attr('href');
     var scrollSize = $(currentSectionId).offset().top - 50;
     $('html, body').animate({
